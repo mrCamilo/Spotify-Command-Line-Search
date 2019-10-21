7d62e8a109d2594d653473197b18a820d1afb095
Manuel Camilo <mcamilo@protonmail.com>
2019-10-20 23:24:08 -0400

    added bandsintown (wip)

--- liri.js
+++ liri.js
@@ -1,46 +1,88 @@
-require("dotenv").config()
-var keys = require("./keys.js");
-var axios = require("axios");
+require("dotenv").config() // dotenv hides the keys
+var keys = require("./keys.js"); //SPOTIFY info is in this file
+var axios = require("axios"); // axios required
 // require("node-spotify-api");
 
 //var spotify = new Spotify(keys.spotify);
 //console.log(spotify);
 
-// var OMDB url
-var movieName = process.argv[2];
-var omdbUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+// var whatUserWantsReturned is used in the 2nd argument (ie. movie-this, concert-this)
+// and determines what API to call from
+// if not a command, like moviethis or concert this, switch statement defaults to 'not a valid command'
+var whatUserWantsReturned = process.argv[2];
 
-// this command is for returning information from the OMDB database
-// the movie information is here
-axios.get(omdbUrl).then( // If the request with axios is successful
-    function(response) {
-    // Title,Year, IMDB Rating, Rotten Tomatoes Rating, Country, Language, Plot,  Actors 
-      console.log("Movie title: " + response.data.Title);
-      console.log("Release year: " + response.data.Year);
-      console.log(response.data.Ratings[0]);
-      console.log(response.data.Ratings[1]);
-      console.log(response.data.Country);
-      console.log("Language: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);
-    })
-    .catch(function(error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    });
+switch (whatUserWantsReturned) {
+    case "movie-this":
+        // OMDB url
+        // process.argv[3] is the name of the movie that was entered in the command line;
+        var theUrl = "http://www.omdbapi.com/?t=" + process.argv[3] + "&apikey=trilogy";
+
+        // this command is for returning information from the OMDB database
+        // the movie information is here
+        axios.get(theUrl).then( // If the request with axios is successful
+            function (response) {
+                // Title,Year, IMDB Rating, Rotten Tomatoes Rating, Country, Language, Plot,  Actors 
+                console.log("Movie title: " + response.data.Title);
+                console.log("Release year: " + response.data.Year);
+                console.log(response.data.Ratings[0]);
+                console.log(response.data.Ratings[1]);
+                console.log(response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+        break;
+    // In this case, return back concert data from BandsInTown!
+    case "concert-this":
+        var theUrl = "https://rest.bandsintown.com/artists/" + process.argv[3] +"/events?app_id=codingbootcamp";
+
+        // this command is for returning information from the bandsintown database
+        // the concert information is here
+        axios.get(theUrl).then( // If the request with axios is successful
+            function (response) {
+                console.log(response.offers);
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+        break;
+    default:
+        console.log("Not a valid command! Try movie-this or concert-this");
+}
