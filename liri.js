4ff049c432ab9d83d237102d7d1fa3c2cf2f1b17
Manuel Camilo <mcamilo@protonmail.com>
2019-10-21 19:49:04 -0400

    fixed the do-what-it-says command

--- liri.js
+++ liri.js
@@ -3,29 +3,10 @@ var keys = require("./keys.js"); //SPOTIFY info is in this file
 var axios = require("axios"); // axios required
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-
-// DO-WHAT-IT-SAYS GOES HERE
-
-if (process.argv[2] === "do-what-it-says")
-{
-// fs is a core Node package for reading and writing files
-var fs = require("fs");
-// This block of code will read from the "random.txt" file.
-fs.readFile("random.txt", "utf8", function (error, textFileData) {
-    // If the code experiences any errors it will log the error to the console.
-    if (error) {
-        return console.log(error);
-    }
-
-    // set the argv[2] equal to what's in the text file
-    process.argv[2] = textFileData();
-});
-}
-// var whatUserWantsReturned is used in the 2nd argument (ie. movie-this, concert-this)
-// and determines what API to call from
-// if not a command, like moviethis or concert this, switch statement defaults to 'not a valid command'
 var whatUserWantsReturned = process.argv[2];
 
+// var whatUserWantsReturned is used in the 2nd argument (ie. movie-this, concert-this) and determines what API to call from
+// if not a command, like moviethis or concert this, switch statement defaults to 'not a valid command'
 switch (whatUserWantsReturned) {
     case "movie-this":
         // OMDB url
@@ -39,9 +20,9 @@ switch (whatUserWantsReturned) {
                 // Title,Year, IMDB Rating, Rotten Tomatoes Rating, Country, Language, Plot,  Actors 
                 console.log("Movie title: " + response.data.Title);
                 console.log("Release year: " + response.data.Year);
-                console.log(response.data.Ratings[0]);
-                console.log(response.data.Ratings[1]);
-                console.log(response.data.Country);
+                console.log(response.data.Ratings[0]); // IMDB Score
+                console.log(response.data.Ratings[1]); // RT Score
+                console.log("Country: " + response.data.Country);
                 console.log("Language: " + response.data.Language);
                 console.log("Plot: " + response.data.Plot);
                 console.log("Actors: " + response.data.Actors);
@@ -75,7 +56,7 @@ switch (whatUserWantsReturned) {
         // the concert information is here
         axios.get(theUrl).then( // If the request with axios is successful
             function (response) {
-                console.log("Event Date" + response.data[0].datetime)
+                console.log("Event Date" + response.data[0].datetime) // this returned undefined when I tried Travis Scott but worked for Weezer
                 console.log("Venue: " + response.data[0].venue.name);
                 console.log("Location: " + response.data[0].venue.city)
             })
@@ -112,6 +93,35 @@ switch (whatUserWantsReturned) {
                 console.log(data.tracks.items[0].name); // log song name
                 console.log(data.tracks.items[0].preview_url); // log preview link (when theres not a preview available it just returns null)
                 console.log(data.tracks.items[0].album.name);// log album title
+            });
+        }
+        break;
+    case "do-what-it-says":
+        {
+            // // fs is a core Node package for reading and writing files
+            var fs = require("fs");
+            // // This block of code will read from the "random.txt" file.
+            fs.readFile("random.txt", "utf8", function (error, textFileData) {
+
+                //     // If the code experiences any errors it will log the error to the console.
+                if (error) {
+                    return console.log(error);
+                }
+
+                // make an array out of the txt file
+                var output = textFileData.split(",");
+
+                // spotify the song
+                spotify.search({ type: 'track', query: output[1] }, function (err, data) {
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+
+                    console.log(data.tracks.items[0].artists[0].name); // Log the artist
+                    console.log(data.tracks.items[0].name); // log song name
+                    console.log(data.tracks.items[0].preview_url); // log preview link (when theres not a preview available it just returns null)
+                    console.log(data.tracks.items[0].album.name);// log album title
+                });
             });
         }
         break;
